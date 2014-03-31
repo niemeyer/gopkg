0f1e91a464f9323ecc95545f06187142bf64a2a8
Gustavo Niemeyer <gustavo@niemeyer.net>
2014-03-31 16:06:12 -0300

    Add version files.

---
+++ version.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Version struct {
+	Major, Minor, Patch int
+}
+
+func (v Version) String() string {
+	if v.Major < 0 {
+		panic(fmt.Sprintf("cannot stringify invalid version (major is %d)", v.Major))
+	}
+	if v.Minor < 0 {
+		return fmt.Sprintf("v%d", v.Major)
+	}
+	if v.Patch < 0 {
+		return fmt.Sprintf("v%d.%d", v.Major, v.Minor)
+	}
+	return fmt.Sprintf("v%d.%d.%d", v.Major, v.Minor, v.Patch)
+}
+
+func (v Version) Less(other Version) bool {
+	if v.Major != other.Major {
+		return v.Major < other.Major
+	}
+	if v.Minor != other.Minor {
+		return v.Minor < other.Minor
+	}
+	return v.Patch < other.Patch
+}
+
+func (v Version) Contains(other Version) bool {
+	if v.Patch != -1 {
+		return v == other
+	}
+	if v.Minor != -1 {
+		return v.Major == other.Major && v.Minor == other.Minor
+	}
+	return v.Major == other.Major
+}
+
+func (v Version) IsValid() bool {
+	return v == InvalidVersion
+}
+
+var InvalidVersion = Version{-1, -1, -1}
+
+func parseVersion(s string) (Version, bool) {
+	if len(s) < 2 {
+		return InvalidVersion, false
+	}
+	if s[0] != 'v' {
+		return InvalidVersion, false
+	}
+	v := Version{-1, -1, -1}
+	i := 1
+	v.Major, i = parseVersionPart(s, i)
+	if i < 0 {
+		return InvalidVersion, false
+	}
+	if i == len(s) {
+		return v, true
+	}
+	v.Minor, i = parseVersionPart(s, i)
+	if i < 0 {
+		return InvalidVersion, false
+	}
+	if i == len(s) {
+		return v, true
+	}
+	v.Patch, i = parseVersionPart(s, i)
+	if i < 0 || i < len(s) {
+		return InvalidVersion, false
+	}
+	return v, true
+}
+
+func parseVersionPart(s string, i int) (part int, newi int) {
+	dot := i
+	for dot < len(s) && s[dot] != '.' {
+		dot++
+	}
+	if dot == i || dot-i > 1 && s[i] == '0' {
+		return -1, -1
+	}
+	for i < len(s) {
+		if s[i] < '0' || s[i] > '9' {
+			return -1, -1
+		}
+		part *= 10
+		part += int(s[i] - '0')
+		i++
+		if i+1 < len(s) && s[i] == '.' {
+			return part, i+1
+		}
+	}
+	return part, i
+}
---
+++ version_test.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	. "launchpad.net/gocheck"
+	"testing"
+)
+
+func Test(t *testing.T) { TestingT(t) }
+
+var _ = Suite(&VersionSuite{})
+
+type VersionSuite struct{}
+
+var versionParseTests = []struct {
+	major, minor, patch int
+	s                   string
+}{
+	{-1, -1, -1, "v"},
+	{-1, -1, -1, "v-1"},
+	{-1, -1, -1, "v01"},
+	{-1, -1, -1, "v1.01"},
+	{-1, -1, -1, "a1"},
+	{-1, -1, -1, "v1a"},
+	{-1, -1, -1, "v1..2"},
+	{-1, -1, -1, "v1.2.3.4"},
+	{-1, -1, -1, "v1."},
+	{-1, -1, -1, "v1.2."},
+	{-1, -1, -1, "v1.2.3."},
+
+	{0, -1, -1,
+		"v0"},
+	{1, -1, -1,
+		"v1"},
+	{1, 2, -1,
+		"v1.2"},
+	{1, 2, 3,
+		"v1.2.3"},
+	{12, 34, 56,
+		"v12.34.56"},
+}
+
+func (s *VersionSuite) TestParse(c *C) {
+	for _, t := range versionParseTests {
+		got, ok := parseVersion(t.s)
+		if t.major == -1 {
+			if ok || got != InvalidVersion {
+				c.Fatalf("version %q is invalid but parsed as %#v", t.s, got)
+			}
+		} else {
+			want := Version{t.major, t.minor, t.patch}
+			if got != want {
+				c.Fatalf("version %q must parse as %#v, got %#v", t.s, want, got)
+			}
+			if got.String() != t.s {
+				c.Fatalf("version %q got parsed as %#v and stringified as %q", t.s, got, got.String())
+			}
+		}
+	}
+}
+
+var versionLessTests = []struct {
+	oneMajor, oneMinor, onePatch int
+	twoMajor, twoMinor, twoPatch int
+	less                         bool
+}{
+	{0, 0, 0, 0, 0, 0, false},
+	{1, 0, 0, 1, 0, 0, false},
+	{1, 0, 0, 1, 1, 0, true},
+	{1, 0, 0, 2, 0, 0, true},
+	{0, 1, 0, 0, 1, 0, false},
+	{0, 1, 0, 0, 1, 1, true},
+	{0, 0, 0, 0, 2, 0, true},
+	{0, 0, 1, 0, 0, 1, false},
+	{0, 0, 1, 0, 0, 2, true},
+}
+
+func (s *VersionSuite) TestLess(c *C) {
+	for _, t := range versionLessTests {
+		one := Version{t.oneMajor, t.oneMinor, t.onePatch}
+		two := Version{t.twoMajor, t.twoMinor, t.twoPatch}
+		if one.Less(two) != t.less {
+			c.Fatalf("version %s < %s returned %v", one, two, !t.less)
+		}
+	}
+}
+
+var versionContainsTests = []struct {
+	oneMajor, oneMinor, onePatch int
+	twoMajor, twoMinor, twoPatch int
+	contains                     bool
+}{
+	{12, 34, 56, 12, 34, 56, true},
+	{12, 34, 56, 12, 34, 78, false},
+	{12, 34, -1, 12, 34, 56, true},
+	{12, 34, -1, 12, 78, 56, false},
+	{12, -1, -1, 12, 34, 56, true},
+	{12, -1, -1, 78, 34, 56, false},
+}
+
+func (s *VersionSuite) TestContains(c *C) {
+	for _, t := range versionContainsTests {
+		one := Version{t.oneMajor, t.oneMinor, t.onePatch}
+		two := Version{t.twoMajor, t.twoMinor, t.twoPatch}
+		if one.Contains(two) != t.contains {
+			c.Fatalf("version %s.Contains(%s) returned %v", one, two, !t.contains)
+		}
+	}
+}
